ecec33ddde29dd8edf184039716717d234902234
NoahYChamp <112650433+NoahYChamp@users.noreply.github.com>
2023-05-02 14:27:29 -0400

    Update dat430_project_2.py
    chart changes

--- dat430_project_2.py
+++ dat430_project_2.py
@@ -8,44 +8,39 @@ data['Year'] = data['Year'].astype(str).str.replace(',', '')
 
 st.title('DAT430 Project 2 Streamlit')
 st.write('By Noah Youngren.')
+st.write('Fundamental Research Question: Has the amount of motor vehicle crash fatalities increased or decreased over time? Which groups are most affected?')
 
-'''
-st.write('This is a description of your fundamental research question and its context.')
 
-st.header('Header')
-'''
-#st.sidebar.write("Sidebar text goes here. Lorem ipsum and all that.")
-#st.sidebar.button('This is a button.')
-
-'''
-#column setter
-col1, col2 = st.columns(2)
-
-with col1:
-  fatal_crashes_over_time = alt.Chart(data).mark_bar().encode(
-      x='Year',
-      y='Fatal Crashes'
-  )
-  st.altair_chart(fatal_crashes_over_time)
-  
-with col2:
-  fatalities_data = data.groupby('Year')[['Driver Fatalities', 'Passenger Fatalities', 'Unknown Occupant Fatalities']].sum().reset_index()
-  fatalities_data = pd.melt(fatalities_data, id_vars=['Year'], var_name='Occupant Type', value_name='Fatalities')
-  stacked_fatalities_chart = alt.Chart(fatalities_data).mark_bar().encode(
-      x='Year',
-      y='Fatalities',
-      color='Occupant Type'
-  )
-
-  st.altair_chart(stacked_fatalities_chart, use_container_width=True)
-'''
 def navigation_menu():
-    options = ['Fatalities by Year', 'Your Next Chart', 'Another Chart']
+    options = ['Total Fatalities by Year', 'Fatalities by Year Breakdown', 'Another Chart']
     selection = st.sidebar.radio('Select a chart:', options)
 
     return selection
 
-def fatalities_by_position_chart():
+def main():
+    st.sidebar.title('Navigation')
+    selection = navigation_menu()
+
+    if selection == 'Fatalities over Time':
+        chart = fatalities_over_time()
+        st.altair_chart(chart, use_container_width=True)
+
+    elif selection == 'Fatalities by Position':
+        chart = fatalities_by_position()
+        st.altair_chart(chart, use_container_width=True)
+
+    elif selection == 'Another Chart':
+        pass
+
+def fatalities_over_time():
+    chart = alt.Chart(data).mark_line().encode(
+        x='Year',
+        y='Total Fatalities'
+    )
+
+    return chart
+      
+def fatalities_by_position():
   fatalities_data = data.groupby('Year')[['Driver Fatalities', 'Passenger Fatalities', 'Unknown Occupant Fatalities']].sum().reset_index()
   fatalities_data = pd.melt(fatalities_data, id_vars=['Year'], var_name='Occupant Type', value_name='Fatalities')
 
@@ -56,18 +51,5 @@ def fatalities_by_position_chart():
     )
   return chart
 
-def main():
-    st.sidebar.title('Navigation')
-    selection = navigation_menu()
-
-    if selection == 'Fatalities by Year':
-        chart = fatalities_by_year_chart()
-        st.altair_chart(chart, use_container_width=True)
-
-    elif selection == 'Your Next Chart':
-        pass
-
-    elif selection == 'Another Chart':
-        pass
 
 main()
