6d4f880602601d44dd90fe9b9b9691339bb7d117
NoahYChamp <112650433+NoahYChamp@users.noreply.github.com>
2023-05-02 13:50:06 -0400

    Update dat430_project_2.py

--- dat430_project_2.py
+++ dat430_project_2.py
@@ -19,19 +19,6 @@ st.sidebar.button('This is a button.')
 #column setter
 col1, col2 = st.columns(2)
 
-#sample data
-Low = 100
-High = 100000
-
-# Compute x^2 + y^2 across a 2D grid
-x, y = np.meshgrid(range(Low, High, 5000), range(100, 0, -10))
-z = x * y
-
-# Convert this grid to columnar data expected by Altair
-model = pd.DataFrame({'x': x.ravel(),
-                     'y': y.ravel(),
-                     'z': z.ravel()})
-
 with col1:
   fatal_crashes_over_time = alt.Chart(data).mark_bar().encode(
       x='Year',
