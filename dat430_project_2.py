48f62baab726659fba29e362093eed489da9590b
NoahYChamp <112650433+NoahYChamp@users.noreply.github.com>
2023-05-02 14:46:04 -0400

    Update dat430_project_2.py

--- dat430_project_2.py
+++ dat430_project_2.py
@@ -3,7 +3,7 @@ import numpy as np
 import altair as alt
 import streamlit as st
 
-ata = pd.read_csv('https://raw.githubusercontent.com/NoahYChamp/DAT430/main/motor_vehicle_data.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/NoahYChamp/DAT430/main/motor_vehicle_data.csv')
 
 st.title('DAT430 Project 2 Streamlit')
 st.write('By Noah Youngren.')
