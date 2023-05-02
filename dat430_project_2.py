791893e738141c78054d61e7651ad677b1ad695e
NoahYChamp <112650433+NoahYChamp@users.noreply.github.com>
2023-05-02 14:01:47 -0400

    Update dat430_project_2.py
    large change, functions and sidebar swapping

--- dat430_project_2.py
+++ dat430_project_2.py
@@ -13,9 +13,10 @@ st.write('This is a description of your fundamental research question and its co
 
 st.header('Header')
 
-st.sidebar.write("Sidebar text goes here. Lorem ipsum and all that.")
-st.sidebar.button('This is a button.')
+#st.sidebar.write("Sidebar text goes here. Lorem ipsum and all that.")
+#st.sidebar.button('This is a button.')
 
+'''
 #column setter
 col1, col2 = st.columns(2)
 
@@ -36,3 +37,38 @@ with col2:
   )
 
   st.altair_chart(stacked_fatalities_chart, use_container_width=True)
+  '''
+def navigation_menu():
+    options = ['Fatalities by Year', 'Your Next Chart', 'Another Chart']
+    selection = st.sidebar.radio('Select a chart:', options)
+
+    return selection
+  
+  
+def main():
+    st.sidebar.title('Navigation')
+    selection = navigation_menu()
+
+    if selection == 'Fatalities by Year':
+        chart = fatalities_by_year_chart()
+        st.altair_chart(chart, use_container_width=True)
+
+    elif selection == 'Your Next Chart':
+        # call function to display your next chart here
+
+    elif selection == 'Another Chart':
+        # call function to display another chart here
+        
+ def fatalities_by_position_chart():
+    fatalities_data = data.groupby('Year')[['Driver Fatalities', 'Passenger Fatalities', 'Unknown Occupant Fatalities']].sum().reset_index()
+    fatalities_data = pd.melt(fatalities_data, id_vars=['Year'], var_name='Occupant Type', value_name='Fatalities')
+
+    chart = alt.Chart(fatalities_data).mark_bar().encode(
+        x='Year',
+        y='Fatalities',
+        color='Occupant Type'
+    )
+
+    return chart
+
+ main()
